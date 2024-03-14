86b17acd031fcc1aa6bd40a06ca978cd717c6885
Woonghwan Koh <ppoox@naver.com>
2024-03-14 13:52:06 +0900

    commit

---
+++ delivery/src/main/java/labshoppubsub/domain/Delivery.java
@@ -0,0 +1,69 @@
+package labshoppubsub.domain;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import labshoppubsub.DeliveryApplication;
+import labshoppubsub.domain.DeliveryAdded;
+import lombok.Data;
+
+@Entity
+@Table(name = "Delivery_table")
+@Data
+//<<< DDD / Aggregate Root
+public class Delivery {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Long orderId;
+
+    private Long productId;
+
+    private Integer qty;
+
+    @PostPersist
+    public void onPostPersist() {
+        DeliveryAdded deliveryAdded = new DeliveryAdded(this);
+        deliveryAdded.publishAfterCommit();
+    }
+
+    public static DeliveryRepository repository() {
+        DeliveryRepository deliveryRepository = DeliveryApplication.applicationContext.getBean(
+            DeliveryRepository.class
+        );
+        return deliveryRepository;
+    }
+
+    //<<< Clean Arch / Port Method
+    public static void addDelivery(OrderPlaced orderPlaced) {
+        //implement business logic here:
+
+        /** Example 1:  new item 
+        Delivery delivery = new Delivery();
+        repository().save(delivery);
+
+        DeliveryAdded deliveryAdded = new DeliveryAdded(delivery);
+        deliveryAdded.publishAfterCommit();
+        */
+
+        /** Example 2:  finding and process
+        
+        repository().findById(orderPlaced.get???()).ifPresent(delivery->{
+            
+            delivery // do something
+            repository().save(delivery);
+
+            DeliveryAdded deliveryAdded = new DeliveryAdded(delivery);
+            deliveryAdded.publishAfterCommit();
+
+         });
+        */
+
+    }
+    //>>> Clean Arch / Port Method
+
+}
+//>>> DDD / Aggregate Root
